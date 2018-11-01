9d11db016292f20c949f2b72c8ade8cd966456c8
Mohamed Mostafa <31463793+foras3zz@users.noreply.github.com>
2018-11-01 22:00:38 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
-// Course:  CS213 - Programming II  - 2018
-// Title:   Assignment II - Task 1
-// Author:  Mohamed Mostafa,Mohamed Kilany,Mohamed Twfiek
-// Version: 1.3
-// Students ID : 20170257,20170254,20170245
-// Student Group : G9
-
 #include <iostream>
-#include"Matrix.h"
+#include"Bigint.h"
+
 using namespace std;
+
 int main()
 {
-    Matrix mat(2,2); //Variable of Type Matrix
+    Bigint a;
+    Bigint b;
+    a-b;
 
-    //Matrix mat2(2,2);//Variable of Type Matrix
 
-
-    cout<<mat.isSymetric();
-
-    return 0;
 }
